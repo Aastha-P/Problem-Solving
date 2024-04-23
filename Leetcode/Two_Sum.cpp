329508ff93f278613777dd3405005f9c480c63e3
Aastha-P <aas12sept22@gmail.com>
2024-04-23 23:27:05 +0530

    Two Sum/Easy

--- Leetcode/Two_Sum.cpp
+++ Leetcode/Two_Sum.cpp
@@ -1,32 +1,34 @@
+// *********By Array**********
+// #include <iostream>
+// int main()
+// {
+//     int target, n;
 
-#include <iostream>
-int main()
-{
-    int target, n;
+//     std::cout << "value of n:";
+//     std::cin >> n;
+//     int num[n];
+//     for (int j = 0; j < n; j++)
+//     {
+//         std::cout << "numbers:";
+//         std::cin >> num[j];
+//     }
 
-    std::cout << "value of n:";
-    std::cin >> n;
-    int num[n];
-    for (int j = 0; j < n; j++)
-    {
-        std::cout << "numbers:";
-        std::cin >> num[j];
-    }
+//     std::cout << "target:";
+//     std::cin >> target;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int k = i + 1; k < n; k++)
+//         {
+//             if (target == (num[i] + num[k]))
+//             {
 
-    std::cout << "target:";
-    std::cin >> target;
-    for (int i = 0; i < n; i++)
-    {
-        for (int k = i + 1; k < n; k++)
-        {
-            if (target == (num[i] + num[k]))
-            {
+//                 std::cout << "[" << i << "," << k << "]";
+//             }
+//         }
+//     }
+//     // delete[] num; // deallocate memory
 
-                std::cout << "[" << i << "," << k << "]";
-            }
-        }
-    }
-    // delete[] num; // deallocate memory
+//     return 0;
+// }
 
-    return 0;
-}
+// ************BY Vector************
