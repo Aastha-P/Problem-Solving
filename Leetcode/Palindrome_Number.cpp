35734fbf09197d49c9f97f2753e84017a3940054
Aastha-P <aas12sept22@gmail.com>
2024-04-24 00:21:54 +0530

    Palindrome Number/Easy

---
+++ Leetcode/Palindrome_Number.cpp
@@ -0,0 +1,27 @@
+// Here we check if a number is palindrome number or not(case - if number is negative it not be a palindrome number)
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, digit = 0, rev = 0;
+    cout << "Number: ";
+    cin >> n;
+    int x = n;
+    while (x != 0)
+    {
+        digit = x % 10;
+        rev = rev * 10 + digit;
+        x = x / 10;
+    }
+
+    if (n >= 0 && n == rev)
+    {
+        cout << "True";
+    }
+    else
+    {
+        cout << "False";
+    }
+
+    return 0;
+}
