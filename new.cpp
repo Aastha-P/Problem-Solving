28297d70993f006ce71c7a10f979abe449e162a5
Aastha-P <aas12sept22@gmail.com>
2024-05-02 18:24:20 +0530

    new

--- new.cpp
+++ new.cpp
@@ -1,7 +1,38 @@
+#include <string>
 #include <iostream>
+#include <vector>
 using namespace std;
-int var = 0;
-namespace Test
+
+int main()
 {
-    int var = 2;
+    string s;
+    int n;
+    vector<string> ss;
+    cout << "No. of words:";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "Words " << i << ": ";
+        cin >> s;
+        ss.push_back(s);
+    }
+    // cout << s.size();
+    // for (int i = 0; i < ss.size(); ++i)
+    // {
+    //     cout << ss[i] << '\n';
+    // }
+
+    for (int j = 0; j < ss.size(); j++)
+        for (int k = 0; k < ss[j].size(); k++)
+        {
+            if (ss[j][k] = ss[j + 1][k])
+            {
+                cout << ss[j][k];
+            }
+            else
+            {
+                cout << "not matched";
+            }
+        }
+    return 0;
 }
