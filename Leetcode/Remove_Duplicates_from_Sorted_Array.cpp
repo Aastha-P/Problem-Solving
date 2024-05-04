1251712dc63973023f94480954d54c1a276d7ea8
Aastha-P <aas12sept22@gmail.com>
2024-05-04 23:05:36 +0530

    Remove Duplicates from Sorted Array/Easy

---
+++ Leetcode/Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        if (nums.size() == 0)
+        {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.size(); j++)
+        {
+            if (nums[i] != nums[j])
+            {
+                i++;
+            }
+            nums[i] = nums[j];
+        }
+        return i + 1;
+    }
+};
+
+int main()
+{
+    vector<int> nums = {3, 4, 3, 5, 2, 6, 3};
+    Solution s;
+    int newNum = s.removeDuplicates(nums);
+
+    cout << "New Size: " << newNum << endl;
+    cout << "Remaining elements: ";
+    for (int i = 0; i < newNum; i++)
+    {
+        cout << nums[i + 1] << ",";
+    }
+    cout << endl;
+
+    return 0;
+}
