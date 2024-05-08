178843f9aa511d079669db164f8ebf8dce25727a
Aastha-P <aas12sept22@gmail.com>
2024-05-08 23:09:43 +0530

    Plus One/Easy

---
+++ Leetcode/Plus_One.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+        int size = digits.size();
+        if (!size)
+            return digits;
+        for (int i = size - 1; i >= 0; --i)
+        {
+            if (digits[i] + 1 > 9)
+            {
+                digits[i] = digits[i] + 1 - 10;
+                if (i == 0)
+                    digits.insert(digits.begin(), 1);
+            }
+            else
+            {
+                digits[i] = digits[i] + 1;
+                break;
+            }
+        }
+        return digits;
+    }
+};
+
+int main()
+{
+    Solution s;
+    return 0;
+}
