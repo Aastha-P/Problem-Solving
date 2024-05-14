672cd166ee3502ccec9089b241eebc013f0eea9e
Aastha-P <aas12sept22@gmail.com>
2024-05-14 23:23:07 +0530

    ghjh

--- Summary_Ranges.cpp
+++ Summary_Ranges.cpp
@@ -29,6 +29,7 @@ public:
                         vector<string> ans;
                         for (int i = 0; i < n; i++)
                         {
+
                             start = nums[i];
                             while (i + 1 < n && nums[i] != INT_MAX && nums[i + 1] == nums[i] + 1)
                             {
