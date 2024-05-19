ca2cad577918c0b4f3b90f7d3b042b73eabdfb1f
Aastha-P <aas12sept22@gmail.com>
2024-05-19 23:34:40 +0530

    Valid Word/Easy

---
+++ Valid_Word.cpp
@@ -0,0 +1,41 @@
+class Solution
+{
+public:
+    bool isValid(string word)
+    {
+        int n = word.size();
+        if (n < 3)
+        {
+            return false;
+        }
+        if (word.size() > 2)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (word[j] == '$' || word[j] == '#' || word[j] == '@')
+                {
+                    return false;
+                }
+            }
+        }
+
+        if (word.size() > 2)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (isdigit(word[j]) || isalpha(word[j]))
+                {
+                    if (word[j] == 'a' || word[j] == 'e' || word[j] == 'i' || word[j] == 'o' || word[j] == 'u')
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+};
