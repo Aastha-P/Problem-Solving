59810b4bd4d7b9404711ddf6f58064c75c4e711a
Aastha-P <aas12sept22@gmail.com>
2024-05-20 21:58:14 +0530

    Valid Word

--- Valid_Word.cpp
+++ Valid_Word.cpp
@@ -1,70 +1,39 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 class Solution
 {
 public:
     bool isValid(string word)
     {
-        int n = word.size();
-        if (n < 3)
+        if (word.length() < 3)
         {
             return false;
         }
-        if (word.size() > 2)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (word[j] == '$' || word[j] == '#' || word[j] == '@')
-                {
-                    return false;
-                }
-            }
-        }
 
-        if (word.size() > 2)
+        bool hasVowel = false;
+        bool hasConsonant = false;
+
+        for (char ch : word)
         {
-            for (int j = 0; j < n; j++)
+            if (!isalnum(ch))
             {
-                if (isdigit(word[j]) || isalpha(word[j]))
-                {
-                    if (word[j] == 'a' || word[j] == 'e' || word[j] == 'i' || word[j] == 'o' || word[j] == 'u')
-                    {
-                        return true;
-                    }
-                }
-                else
-                {
-                    return false;
-                }
+                return false;
+            }
+            if (isdigit(ch))
+            {
+                continue;
+            }
+            char lowerCh = tolower(ch);
+            if (lowerCh == 'a' || lowerCh == 'e' || lowerCh == 'i' || lowerCh == 'o' || lowerCh == 'u')
+            {
+                hasVowel = true;
+            }
+            else
+            {
+                hasConsonant = true;
             }
         }
-        return false;
+        return hasVowel && hasConsonant;
     }
 };
-int main()
-{
-    Solution s;
-    return 0;
-}
-
-// unordered_set<char> s = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
-// int n = word.size();
-// if (n < 3)
-// {
-//     return false;
-// }
-// bool df = false, uf = false, lf = false, vf = false, cf = false;
-// for (auto c : word)
-// {
-//     if (!isdigit(c))
-//         df = true;
-//     if (isdigit(c))
-//         lf = true;
-//     if (islower(c))
-//         uf = true;
-//     if (isupper(c))
-//         vf = true;
-//     else if (!s.count(c) && isalpha(c))
-//         cf = true;
-// }
-// return ((df || uf || lf) && vf & cf);
