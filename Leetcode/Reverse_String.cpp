367d9d0bbba36f091a518f4146506e2cb17d3ec2
Aastha-P <aas12sept22@gmail.com>
2024-05-21 22:13:27 +0530

    Reverse String

---
+++ Leetcode/Reverse_String.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    void reverseString(vector<char> &s)
+    {
+        int l = 0;
+        int r = s.size() - 1;
+        while (l < r)
+        {
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
+    }
+};
+int main()
+{
+    Solution s;
+    return 0;
+}
