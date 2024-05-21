aa40b5aafebc79e6246ec6021234123d16bb162e
Aastha-P <aas12sept22@gmail.com>
2024-05-21 22:30:01 +0530

    Reverse  String/Easy

--- Leetcode/Reverse_String.cpp
+++ Leetcode/Reverse_String.cpp
@@ -1,27 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 class Solution
 {
 public:
+    // void reverseString(vector<char> &s)
+    // {
+    //     int l = 0;
+    //     int r = s.size() - 1;
+    //     while (l < r)
+    //     {
+    //         char temp = s[l];
+    //         s[l] = s[r];
+    //         s[r] = temp;
+    //         l++;
+    //         r--;
+    //     }
+    // }
+
     void reverseString(vector<char> &s)
     {
-        int l = 0;
-        int r = s.size() - 1;
-        while (l < r)
-        {
-            char temp = s[l];
-            s[l] = s[r];
-            s[r] = temp;
-            l++;
-            r--;
-        }
+        reverse(s.begin(), s.end());
     }
 };
 int main()
 {
     Solution so;
-    vector<char> s = {'h', 'e', 'l'};
+    vector<char> s = {'h', 'e', 'l', 'l', 'o'};
     so.reverseString(s);
 
     for (char c : s)
