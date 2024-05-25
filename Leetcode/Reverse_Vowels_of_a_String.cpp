b2d165cf410e329ad1ed6b9afac432434e80a5b5
Aastha-P <aas12sept22@gmail.com>
2024-05-25 23:19:15 +0530

    Reverse Vowels of a String

---
+++ Leetcode/Reverse_Vowels_of_a_String.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    string reverseVowels(string s)
+    {
+        auto isVowel = [](char c)
+        {
+            c = tolower(c);
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        };
+
+        int l = 0;
+        int r = s.size() - 1;
+        while (l < r)
+        {
+            if (isVowel(s[l]) && isVowel(s[r]))
+            {
+                swap(s[l], s[r]);
+                l++;
+                r--;
+            }
+            else
+            {
+                if (!isVowel(s[l]))
+                    l++;
+                if (!isVowel(s[r]))
+                    r--;
+            }
+        }
+        return s;
+    }
+};
