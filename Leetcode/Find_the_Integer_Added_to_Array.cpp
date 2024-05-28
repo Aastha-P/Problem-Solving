169f6fa0d31ae86106a95fb5d3b6a86efa3d80f7
Aastha-P <aas12sept22@gmail.com>
2024-05-28 22:41:58 +0530

    Find the Integer Added to Array

---
+++ Leetcode/Find_the_Integer_Added_to_Array.cpp
@@ -0,0 +1,16 @@
+// Find the Integer Added to Array
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    int addedInteger(vector<int> &nums1, vector<int> &nums2)
+    {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int x = nums2[0] - nums1[0];
+        return x;
+    }
+};
