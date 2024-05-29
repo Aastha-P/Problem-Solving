c2b0b25a2f18bb91a387ac82dee1844f874ab999
Aastha-P <aas12sept22@gmail.com>
2024-05-29 23:47:27 +0530

    Find the Difference

---
+++ Find_the_Difference.cpp
@@ -0,0 +1,15 @@
+// Find the Difference
+
+class Solution
+{
+public:
+    char findTheDifference(string s, string t)
+    {
+        int sum = 0;
+        for (int i = 0; i < t.size(); i++)
+        {
+            sum += t[i] - s[i];
+        }
+        return sum;
+    }
+};
