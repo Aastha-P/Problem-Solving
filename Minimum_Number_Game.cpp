c9246661bb9caa31c903068e1134f4dc7b3dc61e
Aastha-P <aas12sept22@gmail.com>
2024-05-31 22:27:00 +0530

    Minimum Number Game

--- Minimum_Number_Game.cpp
+++ Minimum_Number_Game.cpp
@@ -1,1 +1,13 @@
 // Minimum Number Game
+
+class Solution
+{
+public:
+    vector<int> numberGame(vector<int> &nums)
+    {
+        ranges::sort(nums);
+        for (int i = 0; i < nums.size(); i += 2)
+            swap(nums[i], nums[i + 1]);
+        return nums;
+    }
+};
