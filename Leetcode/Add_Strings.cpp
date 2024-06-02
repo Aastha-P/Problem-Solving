30da1f6c1557931693f01275eaf50aa21efc854a
Aastha-P <aas12sept22@gmail.com>
2024-06-02 23:31:00 +0530

    Add Strings/Easy

--- Leetcode/Add_Strings.cpp
+++ Leetcode/Add_Strings.cpp
@@ -7,29 +7,23 @@ class Solution
 public:
     string addStrings(string num1, string num2)
     {
-        int i = num1.size() - 1;
-        int j = num2.size() - 1;
+        string ans;
         int carry = 0;
-        string result = "";
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
 
         while (i >= 0 || j >= 0 || carry)
         {
-            int sum = carry;
             if (i >= 0)
-            {
-                sum += num1[i] - '0';
-                i--;
-            }
+                carry += num1[i--] - '0';
             if (j >= 0)
-            {
-                sum += num2[j] - '0';
-                j--;
-            }
-            carry = sum / 10;
-            result.push_back(sum % 10 + '0');
+                carry += num2[j--] - '0';
+            ans += carry % 10 + '0';
+            carry /= 10;
         }
-        reverse(result.begin(), result.end());
-        return result;
+
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
 
