26139a538dc91d1c34ac0bcbf36b7c813b9bf581
Aastha-P <aas12sept22@gmail.com>
2024-06-03 23:33:53 +0530

    Ugly Number

---
+++ Leetcode/Ugly_Number.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+class Solution
+{
+public:
+    bool isUgly(int n)
+    {
+        if (n <= 0)
+            return false;
+
+        int prime[] = {2, 3, 5};
+
+        for (int i = 0; i < 3; i++)
+        {
+            while (n % prime[i] == 0)
+                n /= prime[i];
+        }
+
+        return n == 1;
+    }
+};
